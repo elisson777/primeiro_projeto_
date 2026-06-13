eeebdc077e0d75312b209dd2df5e284a2c19664b
elisson777 <nesepedro@gmail.com>
2026-06-12 21:25:25 -0300

    aprendendo_variaveis_e_colocando_na_tela_

---
+++ programa01.py
@@ -0,0 +1,13 @@
+from operator import truediv
+
+nome = "elisson"
+ultimo_nome = "rodrigues"
+idade = 18 #
+altura = 1.75 # float
+ano_nascimento = 2012 # bool
+grana_que_vc_tem = 1000.50 # bool
+maior_de_idade = True
+
+
+print("ola",nome , ultimo_nome, "idade","19" ,altura, )
+
