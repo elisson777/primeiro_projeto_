eef571a50199f4b1008bc7a8f6050135ca636e00
elisson777 <nesepedro@gmail.com>
2026-06-16 21:44:51 -0300

    else corrigido

--- progra de media.py
+++ progra de media.py
@@ -6,18 +6,20 @@ nota3 = float(input("diga uma nota3 "))
 
 media = (nota1 + nota2 + nota3) / 3
 
-if media >= 3 and media < 7:
+if media >= 7:
     print(f"ALUNX aprovado COM MEDIA {media:.2f}")
-else:
+elif media >= 3 and media < 7:
     print(f"alunx em recuperacao com media {media:.2f}")
     fez_recuperacao = input("aluno ja fez recuperacao? s/n: ")
-    if fez_recuperacao == "n":
-    nota3   = float(input("diga uma nota3"))
-    if nota_recuperacao >= 7:
-        print("aluno aprovado pela recuperacao")
+    if fez_recuperacao == "s":
+        nota_recuperacao = float(input("diga uma recuperacao"))
+        if nota_recuperacao >= 5:
+            print("aluno aprovado pela recuperacao")
+        else:
+            print("aluno nao obeteve nota suficiente para ser aprovado pela recuperacao")
     else:
-        print("aluno nao obeteve nota suficiente para ser aprovado pela recuperacao")
+        print("aluno aguardando recuperacao")
 else:
-print(f"aluno reprovado com media {media:.2f}")
+    print(f"aluno reprovado com media {media:.2f}")
 
 
