e9de9ddd22fa73a24656a522f0429b11c3012acf
elisson777 <nesepedro@gmail.com>
2026-06-18 21:14:10 -0300

    atividades

--- atividade par ou inpa.py
+++ atividade par ou inpa.py
@@ -4,3 +4,62 @@ if numero % 2 == 0:
     print("par")
 else:
     print("impar")
+
+#atividade_2
+
+velocimetro =int(input("qual a velocidade mano vey ?"))
+
+if velocimetro >= 80:
+    print("alta pra karalho mano vey")
+else:
+    print("fica deboa")
+
+#atividade_3
+
+chamas = int(input("o fogao ta liga?"))
+
+if chamas < 15:
+    print("clima frio")
+elif chamas >= 25 and chamas <= 35:
+    print("clima agradavel")
+else:
+    print("clima quente ")
+
+#ATIVIDADE_04
+
+ano_nascimento = int(input("fala o ano em que vc nasceu, meno!!!!: "))
+
+idade = 2026 - ano_nascimento
+
+if idade >= 16:
+    print("ta liberado meu patrao")
+elif idade <= 10 and idade >=5:
+    print("negado,meu chefe vai pra casa")
+else:
+    print("vc ta doido maluco vai ver tv globinho")
+
+#atividade_05
+
+salario = float(input("Digite o salário bruto: "))
+parcela = float(input("Digite o valor da parcela: "))
+
+limite = salario * parcela
+
+if parcela <=30:
+    print("Crédito Aprovado")
+else:
+    print("Crédito Recusado")
+
+
+
+#atividade 6
+
+
+
+
+categoria= float(input(" digite sua idade manin"))
+if categoria <= 9:
+    print("mirin")
+elif sua_categoria == 10:
+    print("mirin")
+
