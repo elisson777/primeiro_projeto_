9dde0f0b28184ed618a2658e0024040d2bf8f0d0
elisson777 <nesepedro@gmail.com>
2026-06-26 21:19:49 -0300

    lista vetor

---
+++ pasta ativ/lista de vetor.py
@@ -0,0 +1,24 @@
+lista= ["ana", "elisson" , "bebedragao","gotica", "estrume","golem"]
+print(*lista)
+
+
+
+nome = int(input( "qual nome" ))
+print(lista [nome])
+
+frutas= int(input( "qual frutas" ))
+frutas= ["pera","macao","ovo","abobra","file","tumate","verba","verbaduce"]
+
+\
+
+
+
+
+
+
+
+
+
+
+
+
